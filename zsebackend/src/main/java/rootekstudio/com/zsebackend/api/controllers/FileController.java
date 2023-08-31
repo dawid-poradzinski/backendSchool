715c5de1d5b7afaa8e79542e6703b177ab5f4564
Dawid Poradzinski <dawid.poradzinski@gmail.com>
2023-08-31 18:01:08 +0200

    a

--- zsebackend/src/main/java/rootekstudio/com/zsebackend/api/controllers/FileController.java
+++ zsebackend/src/main/java/rootekstudio/com/zsebackend/api/controllers/FileController.java
@@ -2,7 +2,6 @@ package rootekstudio.com.zsebackend.api.controllers;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
--- zsebackend/src/main/java/rootekstudio/com/zsebackend/api/security/WebSecurityConfig.java
+++ zsebackend/src/main/java/rootekstudio/com/zsebackend/api/security/WebSecurityConfig.java
@@ -31,6 +31,7 @@ public class WebSecurityConfig {
         .requestMatchers("/element/add").hasAnyAuthority("ROLE_ADMIN", "ROLE_WORKER")
         .requestMatchers("/element/delete**").hasAnyAuthority("ROLE_ADMIN", "ROLE_WORKER")
         .requestMatchers("/element/**").permitAll()
+        .requestMatchers("/page/gete/**").permitAll()
         .requestMatchers("/page/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_WORKER")
         .requestMatchers("upload-dir/**").permitAll()
         .anyRequest().authenticated();
